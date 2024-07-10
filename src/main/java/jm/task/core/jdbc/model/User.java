62a30c9e063e1e73ddc899bd6564d7f903ab386f
Samir Bagiev <bagsamitoo@gmail.com>
2024-07-10 23:58:13 +0300

    add hibernate

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,18 +1,19 @@
 package jm.task.core.jdbc.model;
+import javax.persistence.Entity;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Table
+@Entity
+@Table(name = "users")
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "last_name")
     private String lastName;
 
     @Override
@@ -25,10 +26,10 @@ public class User {
                 '}';
     }
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
-    public User() {
+    public User () {
 
     }
 
--- target/test-classes/test.sql
+++
@@ -1,1 +0,0 @@
-insert into users (name, last_name, age) values ('Anna', 'Sokolova', 20);
